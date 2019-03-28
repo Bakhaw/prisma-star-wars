dd50457beabdc6230c3f0583a7c31fc621130d56
Faissal <fhattou@simplon.co>
2019-03-28 20:54:30 +0100

    Rewrited <Bandeau /> to pass dynamic css props (height, sizeMode, src) for background

--- src/components/Bandeau/index.js
+++ src/components/Bandeau/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Image = styled.img`
-  height: auto;
+const Image = styled.div`
+  background-image: ${props => `url('${props.src}')`};
+  background-size: ${props => props.size};
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  height: ${props => props.height};
   width: 100%;
+  @media (max-width: 1000px) {
+    background-attachment: unset;
+    background-size: cover;
+    height: 400px;
+  }
 `;
 
-export default function Bandeau({ description, src }) {
-  return <Image alt={description} src={src} />;
+export default function Bandeau({ description, height, sizeMode, src }) {
+  return <Image alt={description} height={height} size={sizeMode} src={src} />;
 }
